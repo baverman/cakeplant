b18d327b5565b2130553aa5d094463d0e4dd148d
Anton Bobrov <bobrov@vl.ru>
2010-12-14 17:19:55 +1000

    modify test according to grid api changes

--- tgrid.py
+++ tgrid.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 
 import gtk
-from taburet.ui.grid import GridColumn, Grid, IntGridColumn
-from taburet.ui import idle
+from taburet.ui.grid import GridColumn, Grid, IntGridColumn, DirtyRow
+
+from taburet.ui.util import idle
+
+def on_commit(dr, row):
+    if 'isnew' in row:
+        dr.grid.model.append({'aaa':'', 'bbb':'', 'isnew':True})
+        del row['isnew']
+        idle(dr.grid.populate, dr.grid.from_row+1, False, (dr.model_row+1, 0))
+
+def on_error(dr, e):
+    print e
 
 model = [{'aaa': r, 'bbb': 'wow'} for r in range(50)]
-model.append({'aaa':'', 'bbb':''})
+model.append({'aaa':'', 'bbb':'', 'isnew':True})
 
 window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 window.set_type_hint(gtk.gdk.WINDOW_TYPE_HINT_DIALOG)
@@ -16,9 +26,10 @@ sw = gtk.ScrolledWindow()
 sw.set_policy(gtk.POLICY_NEVER, gtk.POLICY_ALWAYS)
 window.add(sw)
 
-table = Grid(IntGridColumn('aaa'), GridColumn('bbb'))
+table = Grid([IntGridColumn('aaa'), GridColumn('bbb')])
 sw.add(table)
-table.set_model(model)
+dr = DirtyRow(table, on_commit, on_error)
+table.set_model(model, dr)
 table.show()
 
 w, h = sw.size_request()

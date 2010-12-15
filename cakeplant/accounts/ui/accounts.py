7cfa16cb30c3353425d17be40ec01a3b5381bc67
Anton Bobrov <bobrov@vl.ru>
2010-12-15 15:17:10 +1000

    возможность добавлять новые счета

--- cakeplant/accounts/ui/accounts.py
+++ cakeplant/accounts/ui/accounts.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
 from taburet.ui import BuilderAware, join_to_file_dir
 from taburet.ui import form
 
-from taburet.accounts import AccountsPlan, accounts_walk
+from taburet.accounts import AccountsPlan, accounts_walk, Account
+from taburet.counter import save_model_with_autoincremented_id
 
 class AccountsForm(BuilderAware):
     """glade-file: accounts.glade"""
@@ -13,20 +15,23 @@ class AccountsForm(BuilderAware):
         self.init_form()
 
     def on_commit(self, dr, row):
-        row.save()
+        path = dr.account_path
+        if row._id:
+            row.save()
+        else:
+            save_model_with_autoincremented_id(row, 'acc')
+            self.account_store[path][0] = row.id
+            self.add_new_account(path)
 
-    def name_changed(self, field, dr):
+    def update_tree_view_value(self, field, dr):
+        columns = {'name':1, 'desc':2}
         it = self.account_store.get_iter(dr.account_path)
-        self.account_store.set(it, 1, dr[field.name])
-
-    def desc_changed(self, field, dr):
-        it = self.account_store.get_iter(dr.account_path)
-        self.account_store.set(it, 2, dr[field.name])
+        self.account_store.set(it, columns[field.name], dr[field.name])
 
     def init_form(self):
         self.dr = form.DirtyRow([
-            form.Field('name', self.name_entry, on_change=self.name_changed),
-            form.Field('desc', self.desc_entry, on_change=self.desc_changed)],
+            form.Field('name', self.name_entry, on_change=self.update_tree_view_value),
+            form.Field('desc', self.desc_entry, on_change=self.update_tree_view_value)],
             self.on_commit
         )
 
@@ -34,22 +39,46 @@ class AccountsForm(BuilderAware):
         roots = {0:None}
         node = None
         plevel = 0
-        for level, acc in accounts_walk(AccountsPlan().accounts()):
-            self.accounts[acc.id] = acc
+        plan = AccountsPlan()
+        for level, acc in accounts_walk(plan.accounts()):
             if level > plevel:
                 roots[level] = node
 
             node = self.account_store.append(roots[level], (acc.id, acc.name, acc.desc))
+            self.accounts[self.account_store.get_path(node)] = acc
             plevel = level
 
-        self.account_view.expand_all()
-        self.window.set_default_size(-1, max(400, min(500, self.account_view.size_request()[1])))
+        def add_new(rows):
+            if not rows:
+                return
+
+            for p in rows:
+                if self.account_store.iter_has_child(p.iter):
+                    self.account_view.expand_row(p.path, False)
+
+                add_new(list(p.iterchildren()))
+                self.add_new_account(p.path)
+
+        add_new(list(self.account_store))
+        self.add_new_account(None)
+        self.window.set_default_size(-1, max(200, min(400, self.account_view.size_request()[1])))
+
+    def add_new_account(self, root):
+        a = Account(name=u'Новый счет')
+        rootiter = None
+
+        if root:
+            a.parents=self.accounts[root].account_path
+            rootiter = self.account_store.get_iter(root)
+
+        node = self.account_store.append(rootiter, (a.id, a.name, a.desc))
+        self.accounts[self.account_store.get_path(node)] = a
 
     def show(self):
         self.window.show_all()
 
     def update_form(self, path):
-        if self.dr.set_row(self.accounts[self.account_store[path][0]]):
+        if self.dr.set_row(self.accounts[path]):
             self.dr.account_path = path
 
     def on_account_view_cursor_changed(self, view):
